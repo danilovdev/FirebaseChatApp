c1783e94421fad18e1e8f537922821bbc62b1a89
Aleksei Danilov <danilovdev@gmail.com>
2018-01-10 00:58:57 +0800

    part 1 completed

---
+++ FirebaseChatApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by Alexey Danilov on 09.01.18.
+//  Copyright © 2018 danilovdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+    }
+    
+    @objc func handleLogout() {
+        let loginController = LoginViewController()
+        present(loginController, animated: true)
+    }
+
+
+}
+
